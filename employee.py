e794156b9104e44eb1eba529f0d27b4a0fd0f255
Adolfo Gonzalez <aagp81@yahoo.es>
2023-12-07 10:01:41 +0800

    first commit import scope

---
+++ employee.py
@@ -0,0 +1,8 @@
+employee_name = "Adolfo"
+age = "42"
+title = "owner"
+
+def details():
+    print("Employee name is:  ", employee_name)
+    print("Employee age is: ", age)
+    print("Employee title is:  ", title)
